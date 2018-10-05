c8f9fe6e2b6c97f8a71f734db90d447abde1c110
AgungEdiKuncoro <42725305+AgungEdiKuncoro@users.noreply.github.com>
2018-10-05 11:18:47 +0700

    Update submit.php

--- submit.php
+++ submit.php
@@ -1,20 +1,18 @@
 <?php
-require_once("db.php");
+    require_once("db.php");
 
-$nama       = $_POST['nama'];
-$nim        = $_POST['nim'];
-$tgl_lahir  = $_POST['tgl_lahir'];
+    $nama       = $_POST['nama'];
+    $nim        = $_POST['nim'];
+    $tgl_lahir  = $_POST['tgl_lahir'];
+    $sql = "INSERT INTO siswa(nama, nim, tgl_lahir) 
+            VALUES ('$nama', '$nim', '$tgl_lahir')";
 
-$sql = "INSERT INTO siswa(nama, nim, tgl_lahir) 
-        VALUES ('$nama','$nim','$tgl_lahir')";
+    if (mysqli_query($conn, $sql)) {
+        echo "Data berhasil ditambahkan!";?>
+        <html><br><a href="list.php">Tampilkan Data</a></html><?php
+    }else {
+        echo "Error: " . $sql . "<br?" . mysqli_error($conn);
+    }
 
-
-if (mysqli_query($conn, $sql)) {
-    echo "New record created successfully<br>";
-} else {
-    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-}
-
-mysqli_close($conn);
-echo "silahkan klik <a href='list.php'>link</a> untuk ke halaman list";
-?> 
+    mysqli_close($conn);
+?>
