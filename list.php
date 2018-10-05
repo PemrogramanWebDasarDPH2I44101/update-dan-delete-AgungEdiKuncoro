22803ea49eca2a99fc5736c53b3db91c9eb01c41
AgungEdiKuncoro <42725305+AgungEdiKuncoro@users.noreply.github.com>
2018-10-05 11:18:19 +0700

    Update list.php

--- list.php
+++ list.php
@@ -1,30 +1,39 @@
 <?php
-require_once("db.php");git 
+    require_once("db.php");
 ?>
-<table border=1>
-    <thead>
-        <th>Nama</th>
-        <th>Nim</th>
-        <th>Tanggal Lahir</th>
-    </thead>
-    <tbody>
-<?php
-$sql    = "SELECT * FROM siswa";
-$result = mysqli_query($conn, $sql);
 
-if (mysqli_num_rows($result) > 0) {
-    // output data of each row
-    while($row = mysqli_fetch_assoc($result)) {
-        echo "<tr>";
-        echo "<td>" . $row["nama"]. "</td>"; 
-        echo "<td>" . $row["nim"]. "</td>";
-        echo "<td>" . $row["tgl_lahir"]. "</td>";
-        echo "</tr>";
-    }
-} else {
-    echo "0 results";
-}
-mysqli_close($conn);
-?> 
-    </tbody>
+<table border=1 style="border-spacing: 0; text-align: center;">
+	<thead>
+		<td>Nama</td>
+		<td>NIM</td>
+		<td>Tanggal</td>
+		<td>Opsi</td>
+	</thead>
+	<tbody>
+		<?php
+		$sql = "SELECT * FROM siswa";
+		$result = mysqli_query($conn, $sql);
+
+		if (mysqli_num_rows($result) > 0) {
+			//output datanya
+			while ($row = mysqli_fetch_assoc($result)) {
+				?>
+					<tr>
+						<td><?php echo $row['nama']?></td>
+						<td><?php echo $row['nim']?></td>
+						<td><?php echo $row['tgl_lahir']?></td>
+						<td><a href="edit.php?id=<?php echo $row['id']?>">Edit</a> | <a href="delete.php?id=<?php echo $row['id']?>">Hapus</a></td>
+					</tr>
+				<?php
+			}
+		}else {
+			echo "<script> 
+					alert('Tidak ada data!'); 
+					location='form.php';
+		 		 </script>";
+		}
+		mysqli_close($conn);
+		?>
+	</tbody>
 </table>
+<a href="form.php">Tambah Data</a>
